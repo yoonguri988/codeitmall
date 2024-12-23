278db08dc7dacdf42e9374c897b4a3557f0b50be
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-12-23 23:30:01 +0900

    Feat: useRouter 쿼리 사용하기

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,3 +1,8 @@
+import { useRouter } from "next/router";
+
 export default function Product() {
-  return <div>Product 페이지</div>;
+  const router = useRouter();
+  // id: 파일이름
+  const { id } = router.query;
+  return <div>Product {id} 페이지</div>;
 }
--- pages/search.js
+++ pages/search.js
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function Search() {
-  return <div>Search 페이지</div>;
+  const router = useRouter();
+  const { q } = router.query;
+  return (
+    <div>
+      <h1>Search 페이지</h1>
+      <h2>{q} 검색결과</h2>
+    </div>
+  );
 }
