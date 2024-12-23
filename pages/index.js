59f2bcd0046439f2d94bd80edee0efcdaafad714
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2024-12-23 23:22:19 +0900

    Feat: Link 컴포넌트

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 import style from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1 className={style.title}>안녕 Next.js</h1>;
+  return (
+    <>
+      <h1>codeitmall</h1>
+      <ul>
+        <li>
+          <Link href="/products/1">첫번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/2">두번째 상품</Link>
+        </li>
+        <li>
+          <Link href="/products/3">세번째 상품</Link>
+        </li>
+        <li>
+          <Link href="https://codeit.kr">코드잇</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
